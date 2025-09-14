716794afc35867f7cb3854465c4f9396f61e14c5
Alejandro-Melgar77 <ale.melgararteaga@gmail.com>
2025-09-14 10:45:46 -0400

    primer ciclo

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()  # 👈 añadimos campo token
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'first_name', 'last_name', 'token')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
+
+    def get_token(self, user):
+        refresh = RefreshToken.for_user(user)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
